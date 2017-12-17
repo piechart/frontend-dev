7054767a6bab2b2a899de4b208a076adc6d8985b
Michael Galperin <mgalp.connect@gmail.com>
2017-12-17 13:12:11 +0300

    Made consts list

--- final/task1/main.py
+++ final/task1/main.py
@@ -1,12 +1,19 @@
 import sys, os, re
 
+###
+
 outer_filename = "output.html"
+forbidden_url_replacement = "[forbidden url]"
+url_regexp = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+acceptable_extensions = ('html', 'css')
+
+###
 
 if len(sys.argv) == 1 or len(sys.argv) == 3:
-    print("Usage: task1.py file.html link_to_find link_to_replace_with blacklist.txt")
+    print("Usage: task1.py file.html url_to_find url_to_replace_with blacklist.txt")
     sys.exit(1)
 
-# Retrive filepath
+# Retrieve filepath
 path = sys.argv[1]
 
 # Check file satisfies all requirements
@@ -14,8 +21,8 @@ if not os.path.exists(path):
     print("File not found")
     sys.exit(1)
 
-if not (path.endswith('.html') or path.endswith('.css')):
-    print("Only html/css files acceptable")
+if not path.endswith(acceptable_extensions):
+    print(f"File extension is not on acceptable list: {acceptable_extensions}")
     sys.exit(1)
 
 # Read file
@@ -35,7 +42,7 @@ if len(sys.argv) == 5:
         f.close()
 
 # Find urls
-urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', data)
+urls = re.findall(url_regexp, data)
 
 # Make frequency dict
 freq = { url: data.count(url) for url in urls }
@@ -44,7 +51,7 @@ print(freq)
 need_save = False
 
 # Perform replacement
-if 'find' in locals():
+if 'find' in locals(): # check if variable exists
     need_save = True
     data = data.replace(find, replace_with)
 
@@ -52,7 +59,7 @@ if 'find' in locals():
 if 'blacklist' in locals():
     need_save = True
     for blacklisted_url in blacklist.split():
-        data = data.replace(blacklisted_url, '[forbidden link]')
+        data = data.replace(blacklisted_url, forbidden_url_replacement)
 
 # Store updated file if needed
 if need_save:
